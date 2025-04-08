e2d89b9a8fddd6b679694a5dbaec69d2836933d5
Erick <erick.nahuelpaiva.001@gmail.com>
2025-04-08 12:35:18 -0300

    organizado en carpeta

--- 1 - Secuenciales/ej2secuencial.js
+++ 1 - Secuenciales/ej2secuencial.js
@@ -4,9 +4,7 @@ const leer = require("prompt-sync")();
  * Funcion principal del programa
  */
 function main() {
-    let contenedor;
-    console.log("ESCRIBIR");
-    contenedor = leer();
+    
 }
 
 main();
