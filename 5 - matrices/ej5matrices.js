c0fba4919ac6dc54a77cd22564355326c3b1e1bf
Erick <erick.nahuelpaiva.001@gmail.com>
2025-05-23 19:02:30 -0300

    ej 5 termnado

--- 5 - matrices/ej5matrices.js
+++ 5 - matrices/ej5matrices.js
@@ -1,6 +1,34 @@
 const leer = require("prompt-sync")();
 
+const nombresDias = ["Lunes", "Martes", "Miercoles", "Jueves", "Viernes", "Sabado", "Domingo"];
+
 function main() {
+    const tareasSemana = [
+        ["Lavar ropa", "Jugar con mascota"],
+        [],
+        ["Ordenar cuarto", "Cocinar", "Ejercicio"],
+        [],
+        ["Practicar programacion"],
+        [],
+        [],
+    ];
+
+    console.log("***Mostrando todos los días de la semana***");
+
+    for (let fila = 0; fila < tareasSemana.length; fila++) {
+        if (tareasSemana[fila].length > 0) {
+            console.log("- " + nombresDias[fila] + ":");
+        }
+        /* 
+        console.log("- " + nombresDias[fila] + ":");
+        if (tareasSemana[fila].length <= 0) {
+            console.log("No hay tareas");
+        }
+        */
+        for (let col = 0; col < tareasSemana[fila].length; col++) {
+            console.log("\t" + (col + 1) + ". " + tareasSemana[fila][col]);
+        }
+    }
 
 }
 
